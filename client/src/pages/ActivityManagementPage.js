dce4c837b37324a47e884fd4cb6d0b8c0156a211
Ali-Mashni <mashnialii@gmail.com>
2025-04-18 22:11:48 +0300

    integrat with admin page

--- client/src/pages/ActivityManagementPage.js
+++ client/src/pages/ActivityManagementPage.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import TopBar from '../components/TopBar';
 import TopBarButton from '../components/TopBarButton';
 
