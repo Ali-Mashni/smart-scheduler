68451fced004f95cc36db833a9eef7406e00f255
haitham20203 <hitham.x9@gmail.com>
2025-04-18 18:59:55 +0300

    Add Admin Dashboard with modals for Account Settings, Ticket Management, and Statistics

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import HomePage from './pages/HomePage';
+import AdminDashboard from './pages/AdminDashboard';
+
 function App() {
-  
+
 
   return (
     <Router>
       <Routes>
         {/* Default redirect to login */}
-        <Route path="/" element={<HomePage />} />        
+        <Route path="/" element={<HomePage />} />
         {/* Auth pages */}
-        <Route path="/login" element={<LoginPage  />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        {/* Admin Dashboard */}
+        <Route path="/admin" element={<AdminDashboard />} />
 
         {/* You can add other pages here later */}
       </Routes>
