c7ae1c0a19738efb87727ad35a2716e335991a85
Ali-Mashni <mashnialii@gmail.com>
2025-05-03 17:31:41 +0300

    Prepare backend for Render deployment

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:'http://localhost:5050'
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 // Attach JWT from localStorage on every request
